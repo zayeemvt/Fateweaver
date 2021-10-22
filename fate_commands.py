3772e2d3613923a0ed816d6d7d3b56575bd310a2
Zayeem Zaman <zayeem@vt.edu>
2021-10-22 12:11:30 -0400

    Added error handling and restructured commands

--- fate_commands.py
+++ fate_commands.py
@@ -1,84 +1,75 @@
+import discord
 from discord.ext import commands
 
 from tarot_deck import Deck
 from tarot_deck import Diviner
 
-def setup(bot: commands.Bot):
-    bot.add_cog(Fateweaver(bot))
+class Player(Diviner):
+    def __init__(self, player: discord.Member):
+        self.id = player
+        self.deck = Deck()
+
 
 class Fateweaver(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
-        self.deck_list = []
-        self.diviner_list = []
-        self.player_map = {}
+        self.player_list = []
         self.tabletop_channel = None
 
     @commands.Cog.listener()
     async def on_ready(self):
         print(f'{self.bot.user.name} connected')
+        await self.bot.change_presence(activity=discord.Game(name=f"{self.bot.command_prefix}help"))
+
+    @commands.command(name="ping")
+    async def ping(self,ctx: commands.Context):
+        await ctx.send(f"Pong! {round(self.bot.latency * 1000)}ms")
 
     @commands.command()
-    async def ping(self,ctx):
-        await ctx.channel.send("pong")
-
-    @commands.command()
-    async def echo(self, ctx, *args):
+    async def echo(self, ctx: commands.Context, *args):
         response = ""
         
         for arg in args:
             response = response + " " + arg
 
-        await ctx.channel.send(response)
+        await ctx.send(response)
 
-    @commands.command()
-    async def register(self, ctx, *args):
-        num_arg_error = False
+    @commands.command(name="set")
+    @commands.has_guild_permissions(administrator=True)
+    async def setChannel(self, ctx: commands.Context, destination: str, channel: discord.TextChannel):
+        if(destination == "tabletop"):
+            self.tabletop_channel = channel
+            await ctx.send("Tabletop channel successfully set.")
 
-        if (len(args) > 0):
-            if (args[0] == "tabletop"):
-                if(ctx.author.guild_permissions.administrator):
-                    if(len(args) == 2):
-                        channel = None
-                        channel_id = args[1].replace('<#','').replace('>','')
-
-                        if (channel_id.isnumeric()):
-                            channel = ctx.bot.get_channel(int(channel_id))
-
-                        if (channel != None):
-                            self.tabletop_channel = channel
-                            await ctx.send("Tabletop channel successfully set.")
-                        else:
-                            await ctx.send("**ERROR**: Invalid channel.")
-
-                    else:
-                        num_arg_error = True
-                else:
-                    await ctx.send("**ERROR**: You do not have permission to use this command.")
-            
-            elif (args[0] == "player"):
-                player_id = ctx.author.id
-
-                if player_id not in self.player_map:
-                    self.player_map[player_id] = len(self.player_map)
-                    self.diviner_list.append(Diviner())
-                    self.deck_list.append(Deck())
-                    await ctx.send(f"Registered player {ctx.author.name}")
-
-                else:
-                    await ctx.send(f"**ERROR**: Player {ctx.author.name} already registered")
-
-            else:
-                await ctx.send("**ERROR**: Invalid argument")
+    @setChannel.error
+    async def set_error(self, ctx: commands.Context, error: commands.CommandError):
+        if isinstance(error, commands.MissingPermissions):
+            message = "You do not have permission to use this command."
         else:
-            num_arg_error = True
+            message = str(error)
 
-        if (num_arg_error):
-            await ctx.send("**ERROR**: Insufficient number of arguments")
+        await ctx.send("**ERROR**: " + message)
+
+
+    @commands.command(name="register")
+    async def registerPlayer(self, ctx: commands.Context):
+        registered_players = [player.id for player in self.player_list]
+
+        if ctx.author not in registered_players:
+            new_player = Player(ctx.author)
+            self.player_list.append(new_player)
+            await ctx.send(f"Registered player {ctx.author}")
+
+        else:
+            await ctx.send(f"**ERROR**: Player {ctx.author} already registered")
 
     @commands.command()
-    async def play(self, ctx, *args):
+    async def play(self, ctx: commands.Context, *args):
         if (self.tabletop_channel == None):
             await ctx.send("**ERROR**: Tabletop channel not set.")
         else:
             await self.tabletop_channel.send("Test")
+
+# For loading Cog
+def setup(bot: commands.Bot):
+    bot.add_cog(Fateweaver(bot))
